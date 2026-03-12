a960e211b00240d4a8fb9c34e078c5f7bcdf7fcb
Bharat Raja Savya Sachi Madishetty <bharatraja.madishetty@gmail.com>
2026-03-12 16:36:07 +0530

    Recursion and Anagram logics

--- src/AnagramString.java
+++ src/AnagramString.java
@@ -2,20 +2,21 @@ import java.util.Arrays;
 
 public class AnagramString {
     public static void main(String []args) {
-        String a = "Mother In Law";
-        String b = "Hitler Woman";
-        a = a.replaceAll(" ","");
-        b = b.replaceAll(" ","");
+        String a = "Mother   In Law ";
+        String b = "    Hitler      Woman";
         a = a.toLowerCase();
         b = b.toLowerCase();
+        // remove white spaces and tab spaces from string
+        a = a.replaceAll("\\s*", "");
+        b = b.replaceAll("\\s*", "");
         char[] c = a.toCharArray();
         char[] d = b.toCharArray();
         Arrays.sort(c);
         Arrays.sort(d);
-        if(c.length == d.length && Arrays.equals(c,d)) {
-            System.out.println("The strings "+a+" and "+b+" are anagram");
-        }else{
-            System.out.println("The strings "+a+" and "+b+" are not anagram");
+        if (Arrays.equals(c, d)) {
+            System.out.println("The strings \"" + a + "\" and \"" + b + "\" are anagrams");
+        } else {
+            System.out.println("The strings \"" + a + "\" and \"" + b + "\" are not anagrams");
         }
     }
 
--- src/MultiplyDigits.java
+++ src/MultiplyDigits.java
@@ -4,21 +4,20 @@
 import java.util.Scanner;
 
 public class MultiplyDigits {
- public static int getProduct(int n) {
+    public static int getProduct(int n) {
         int a = 1;
         int c = 0;
         if(n > 9) {
             while(n > 0) {
                 a = a*(n%10); 
-                n = n/10;     
+                n = n/10;                    
                 if(n == 0) {
                     c++;
+                    System.out.println("Product of all digits = "+a); 
                     if(a > 9) {
                         n = a;
                         a = 1;
-                        System.out.println("digits product = "+n);
                     }else{
-                        System.out.println("digits product = "+a);
                         break;
                     }
                 }
@@ -26,12 +25,27 @@ public class MultiplyDigits {
         }
         return c;
     }
+
+    //Using Recursion
+    private static int getProductRecursion(int n, int c){
+        if(n > 9) {
+            int x = n/10;
+            int y = n%10; 
+            n = x * y;  
+            if(x > 0){
+                System.out.println(x+" * "+y+" = "+n);
+                return getProductRecursion(n, ++c);
+            }           
+        }
+        return c;
+    }
     public static void main(String []args) {
         Scanner sc  = new Scanner(System.in);
         System.out.println("Enter the number: ");
         int n = sc.nextInt();
-        int count = getProduct(n);
-        System.out.println("Final Result = "+count);
+        System.out.println("Final Result = "+getProduct(n));
+        System.out.println("---------Printing using recursion----------");
+        System.out.println("Final Result using recursion = "+getProductRecursion(n,0));
     }
 
 }
