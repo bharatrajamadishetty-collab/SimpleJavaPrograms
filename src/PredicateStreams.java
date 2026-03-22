4ffbdb439bf7a6a6d567871f7702d431d622d07a
Bharat Raja <bharatraja.madishetty@gmail.com>
2026-03-22 18:41:24 +0530

    Using flatmap for List Set Map and Queue Streams

--- src/PredicateStreams.java
+++ src/PredicateStreams.java
@@ -1,7 +1,9 @@
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
 import java.util.Scanner;
+import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
@@ -47,14 +49,32 @@ record User(int id, String name, String role) {}
                             System.out.println("User found with Id: " + u.get().id() + " name: " + u.get().name() + " role: " + u.get().role());
                         }
         }
-
+        
+        //Using flatmap to flatten list elements
         List<List<String>> nameList = Arrays.asList(
-                Arrays.asList("Alice", "Bob"),
-                Arrays.asList("David", "Charlie"),
-                Arrays.asList("Clarke", "Peter")
+                Arrays.asList("Alice", "James"),
+                Arrays.asList("Clarke", "Henry"),
+                Arrays.asList("David", "Peter")
         );
-        //using flatmap to flatten list
         List<String> names = nameList.stream().flatMap(x -> x.stream()).collect(Collectors.toList());
         System.out.println("Names List Pair : " + nameList);
         System.out.println("Flattened Names List : " + names);
+
+        Set<List<String>> s = new HashSet<>();
+        s.add(Arrays.asList("Alice", "Henry"));
+        s.add(Arrays.asList("James", "Charlie"));
+        Set<String> flatSet = s.stream().flatMap(y -> y.stream()).collect(Collectors.toSet());
+        System.out.println("Flattened Names Set = "+flatSet);
+
+        Map<Integer,List<String>> mp = new HashMap<>();
+        mp.put(1,Arrays.asList("Clarke", "Peter"));
+        mp.put(2,Arrays.asList("David", "Henry"));
+        List<String> flatList = mp.values().stream().flatMap(z -> z.stream()).collect(Collectors.toList());
+        System.out.println("Flattened map values = "+flatList);
+
+        Queue<List<String>> q = new ConcurrentLinkedQueue<>();
+        q.add(Arrays.asList("Peter", "Bob"));
+        q.add(Arrays.asList("Henry", "James"));
+        List<String> flatQueue = q.stream().flatMap(f -> f.stream()).collect(Collectors.toList());
+        System.out.println("Flattened queue list = "+flatQueue);
     }
