867b89e480516ef3143af9cfe7befde1ec3d412b
Bharat Raja <bharatraja.madishetty@gmail.com>
2026-04-02 02:24:45 +0530

    Reverse char array and remove NaN from string array using stream filter forEach and str.matches

--- src/RemoveNaNPrintIntegerArray.java
+++ src/RemoveNaNPrintIntegerArray.java
@@ -1,45 +1,86 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
 
 public class RemoveNaNPrintIntegerArray {
-    static Integer[] numbersOnly(String[] d) {
-        Integer[] i = new Integer[d.length];
-        int j = 0;
-        for (String x : d) {
+    static List<Integer> numbersOnly(String[] a) {
+        List<Integer> l = new ArrayList<>();
+        for (String x : a) {
             try {
-                if (!Double.isNaN((Double.parseDouble(x)))) {
-                    i[j++] = Integer.parseInt(x);
+                if (x != null && !Double.isNaN((Double.parseDouble(x)))) {
+                    l.add(Integer.parseInt(x));
                 }
             } catch (NumberFormatException e) {
+            }
+        }
+        return l;
+    }
+
+    static List<Integer> integersOnly(String[] b) {
+        List<Integer> i = new ArrayList<>();
+        for (String x : b) {
+            try {
+                i.add(Integer.parseInt(x));
+            } catch (NullPointerException | NumberFormatException e) {
             }
         }
         return i;
     }
 
+    static List<Integer> integersOnlyUsingMatchers(String[] c) {
+        List<Integer> k = new ArrayList<>();
+        for (String x : c) {
+            try {
+                if (x != null && x.matches("-?\\d+")) {
+                    k.add(Integer.valueOf(x));
+                }
+            } catch (NumberFormatException ex) {
+            }
+        }
+        return k;
+    }
+
     static Integer[] numbersOnlyWithStreams(String[] d) {
         return Arrays.stream(d).filter(y -> {
             try {
-                return !Double.isNaN((Double.parseDouble(y)));
+                return y != null && !Double.isNaN((Double.parseDouble(y)));
             } catch (NumberFormatException e) {
                 return false;
             }
         }).map(Integer::parseInt).toArray(Integer[]::new);
     }
 
-    static void numbersOnlyWithStreamsForEach(String[] t) {
-        Arrays.stream(t).forEach(y -> {
+    static List<Integer> numbersOnlyWithStreamsForEach(String[] t) {
+        List<Integer> j = new ArrayList<>();
+        Stream.of(t).forEach(y -> {
             try {
-                if (!Double.isNaN((Double.parseDouble(y)))) {
-                    System.out.print(y + " ");
+                j.add(Integer.parseInt(y));
+            } catch (NullPointerException | NumberFormatException e) {
+            }
+        });
+        return j;
+    }
+
+    static List<Integer> numberOnlyUsingMatchers(String[] f) {
+        List<Integer> q = new ArrayList<>();
+        Stream.of(f).forEach(g -> {
+            try {
+                if (g != null && g.matches("-?\\d+")) {
+                    q.add(Integer.valueOf(g));
                 }
             } catch (NumberFormatException e) {
             }
         });
+        return q;
     }
 
     public static void main(String[] args) {
-        String[] s = { "193", "as", "380", "413", "9ra", "www", "-52", "NaN", "0" };
-        System.out.println("Integer array after removing NaN values = " + Arrays.toString(numbersOnly(s)));
+        String[] s = { "193", "as", "380", "413", "9ra", "www", "-52", "NaN", null, "null", "0" };
+        System.out.println("Integer array after removing NaN values = " + numbersOnly(s));
+        System.out.println("Integer array after removing NaN values = " + integersOnly(s));
         System.out.println("Integer array after removing NaN values = " + Arrays.toString(numbersOnlyWithStreams(s)));
-        numbersOnlyWithStreamsForEach(s);
+        System.out.println("Integer array after removing NaN values = " + numbersOnlyWithStreamsForEach(s));
+        System.out.println("Integer array after removing NaN values = " + numberOnlyUsingMatchers(s));
     }
 }
--- src/ReverseCharArrayWithTwoPointer.java
+++ src/ReverseCharArrayWithTwoPointer.java
@@ -1,5 +1,7 @@
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.stream.IntStream;
 
 public class ReverseCharArrayWithTwoPointer {
@@ -14,6 +16,23 @@ public class ReverseCharArrayWithTwoPointer {
             j--;
         }
         return s;
+    }
+
+    static char[] reverseChars(char[] s) {
+        for (int i = 0; i < s.length / 2; i++) {
+            char t = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = t;
+        }
+        return s;
+    }
+
+    static char[] reverseCharRead(char[] s) {
+        char[] r = new char[s.length];
+        for (int i = 0; i < s.length; i++) {
+            r[i] = s[s.length - 1 - i];
+        }
+        return r;
     }
 
     static char[] reverseArrayWithRecursion(char[] a, int i, int j) {
@@ -33,13 +52,25 @@ public class ReverseCharArrayWithTwoPointer {
         return sb.toString().toCharArray();
     }
 
-    static void reverseArrayUsingStreamsForEach(char[] d) {
+    static char[] reverseArrayUsingStreamsForEach(char[] d) {
         IntStream.range(0, d.length / 2).forEach(y -> {
             char t = d[y];
             d[y] = d[d.length - 1 - y];
             d[d.length - 1 - y] = t;
         });
-        System.out.println("Reversed char array = " + Arrays.toString(d));
+        return d;
+    }
+
+    static List<Character> reversewithStreamMap(char[] arr) {
+        return IntStream.range(0, arr.length).mapToObj(i -> arr[arr.length - 1 - i]).toList();
+    }
+
+    static List<Character> reverseWithStreamForEach(char[] arr) {
+        List<Character> l = new ArrayList<>();
+        IntStream.range(0, arr.length).forEach(i -> {
+            l.add(arr[arr.length - 1 - i]);
+        });
+        return l;
     }
 
     static char[] reverseArrayUsingStreamsMap(char[] e) {
@@ -51,10 +82,15 @@ public class ReverseCharArrayWithTwoPointer {
         char[] c = { 'h', 'e', 'l', 'l', 'o' };
         System.out.println("Reversed char array = " + Arrays.toString(reverseArrayWithStringBuilder(c)));
         System.out.println("Reversed char array = " + Arrays.toString(reverseString(c)));
+        char[] d = { 'h', 'e', 'l', 'l', 'o' };
+        System.out.println("Reversed char array = " + Arrays.toString(reverseChars(d)));
         System.out.println("Reversed char array = " + Arrays.toString(reverseArrayWithRecursion(c, 0, c.length - 1)));
         char[] f = { 'h', 'e', 'l', 'l', 'o' };
-        reverseArrayUsingStreamsForEach(f);
+        System.out.println("Reversed char array = " + Arrays.toString(reverseArrayUsingStreamsForEach(f)));
         char[] g = { 'h', 'e', 'l', 'l', 'o' };
         System.out.println("Reversed char array = " + Arrays.toString(reverseArrayUsingStreamsMap(g)));
+        System.out.println("Reversed char array = " + reversewithStreamMap(g));
+        System.out.println("Reversed char array = " + reverseWithStreamForEach(g));
+        System.out.println("Reversed char array = " + Arrays.toString(reverseCharRead(g)));
     }
 }
