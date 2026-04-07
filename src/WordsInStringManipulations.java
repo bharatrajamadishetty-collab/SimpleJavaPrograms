b5035f69666e0955bd221ea56546dde3b4f263ac
Bharat Raja <bharatraja.madishetty@gmail.com>
2026-04-08 02:39:25 +0530

    Reversing words and characters in a string and finding largest second largest strings

---
+++ src/WordsInStringManipulations.java
@@ -0,0 +1,62 @@
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class WordsInStringManipulations {
+        static String reverseChararactersInEachWord(String a) {
+                return Stream.of(a.split("\\s+")).map(w -> new StringBuffer().append(w).reverse())
+                                .collect(Collectors.joining(" "));
+        }
+
+        static String wordsInReverseSequenceOrder(String b) {
+                return Stream.of(b.split("\\s+")).collect(Collectors.collectingAndThen(Collectors.toList(), list -> {
+                        Collections.reverse(list);
+                        return String.join(" ", list);
+                }));
+        }
+
+        static String wordsInReverseSequenceUsingIntStream(String b) {
+                String[] t = b.split("\\s+");
+                return IntStream.range(0, t.length).mapToObj(j -> t[t.length - 1 - j]).collect(Collectors.joining(" "));
+        }
+
+        static String reverseWordsAndCharacters(String c) {
+                String g = Stream.of(c.split("\\s+")).map(z -> new StringBuilder(z).reverse())
+                                .collect(Collectors.joining(" "));
+                String[] h = g.split("\\s+");
+                return IntStream.range(0, h.length).mapToObj(k -> h[h.length - 1 - k]).collect(Collectors.joining(" "));
+        }
+
+        public static void main(String[] args) {
+                String s = "I am a      software developer.";
+                Optional<String> secondLargest = Arrays.stream(s.replaceAll("[^a-zA-Z\\s]", "").split("\\s+"))
+                                .sorted(Comparator.comparingInt(String::length).reversed()).skip(1).findFirst();
+                secondLargest.ifPresent(word -> System.out.println("Second Largest word : " + word));
+
+                Optional<Integer> second = Arrays.stream(s.split("[^a-zA-Z\\\\s+]")).map(i -> i.length())
+                                .sorted(Comparator.reverseOrder()).skip(1).findFirst();
+                second.ifPresent(length -> System.out.println("Length of second largest word : " + length));
+
+                Optional<String> first = Arrays.stream(s.split("[^a-zA-Z\\\\s+]"))
+                                .sorted(Comparator.comparingInt(String::length).reversed()).findFirst();
+                first.ifPresent(length -> System.out.println("Largest word : " + length));
+
+                Optional<Integer> max = Arrays.stream(s.split("[^a-zA-Z\\\\s+]")).map(i -> i.length())
+                                .max(Comparator.naturalOrder());
+                max.ifPresent(length -> System.out.println("Length of largest word : " + length));
+
+                System.out.println(
+                                "Printing all reversed characters in each word : " + reverseChararactersInEachWord(s));
+                System.out.println("Printing all words in reverse sequence order : " + wordsInReverseSequenceOrder(s));
+                System.out.println("Printing all words in reverse sequence order : "
+                                + wordsInReverseSequenceUsingIntStream(s));
+                System.out.println("Printing all words in reverse sequence order with all characters also reversed : "
+                                + reverseWordsAndCharacters(s));
+
+        }
+
+}
