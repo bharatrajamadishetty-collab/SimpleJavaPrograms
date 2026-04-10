15054303ab1e0b13b5cd94faf1e704b495a9098e
Bharat Raja <bharatraja.madishetty@gmail.com>
2026-04-11 01:27:44 +0530

    Executors multi implementation returning Future values

--- src/FutureExecutorService.java
+++ src/FutureExecutorService.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class FutureExecutorService {
-        private static long factorial(int n) {
+    private static long factorial(int n) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -17,19 +17,19 @@ public class FutureExecutorService {
         return result;
     }
 
-    private static int increment(int i,int n) {
+    private static Integer increment(int i, int n) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-        while(i <= n) {
+        while (i <= n) {
             i++;
         }
         return i;
     }
 
-    private static String getSubstring(String str,int start,int end) {
+    private static String getSubstring(String str, int start, int end) {
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
@@ -37,26 +37,20 @@ public class FutureExecutorService {
         }
         return str.substring(start, end);
     }
-    
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newFixedThreadPool(3);
-        Future<?> future1 = executorService.submit(() -> {
-            System.out.println("Incremented Value = " + increment(0,9));
-            System.out.println("Task1 is done by Thread : " + Thread.currentThread().getName());
-            });
+        Future<String> future1 = executorService.submit(() -> "Incremented Value = " + increment(0, 9).toString()
+                + " , Task1 is done by Thread : " + Thread.currentThread().getName());
+        System.out.println(future1.get());
 
-        Future<?> future2 = executorService.submit(() -> {
-            System.out.println("Substring Result = " + getSubstring("Delicious!", 2, 9));
-            System.out.println("Task2 is done by Thread : " + Thread.currentThread().getName());
-            });
-
+        Future<String> future2 = executorService.submit(() -> "Substring Result = " + getSubstring("Delicious!", 2, 9)
+                + " , Task2 is done by Thread : " + Thread.currentThread().getName());
+        System.out.println(future2.get());
         Future<?> future3 = executorService.submit(() -> {
             System.out.println("Factorial Result = " + factorial(6));
             System.out.println("Task3 is done by Thread : " + Thread.currentThread().getName());
-            });
-
-        future1.get();
-        future2.get();
+        });
         future3.get();
 
         executorService.shutdown();
