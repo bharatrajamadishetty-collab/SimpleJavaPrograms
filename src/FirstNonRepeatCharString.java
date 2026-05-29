26dbceccd526ef5f8e2d9a0db760c47adfda7828
Bharat Raja Savya Sachi Madishetty <bharatraja.madishetty@gmail.com>
2026-05-29 21:28:29 +0530

    First non repeated character using streams

--- src/FirstNonRepeatCharString.java
+++ src/FirstNonRepeatCharString.java
@@ -1,26 +1,39 @@
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class FirstNonRepeatCharString {
-    public static void main(String []args) {
+
+    static String firstNonRepeatCharUsingStreams(String s) {
+        return Stream.of(s.split(""))
+                .collect(Collectors.groupingBy(Function.identity(), LinkedHashMap::new, Collectors.counting()))
+                .entrySet().stream().filter(f -> f.getValue() == 1).findFirst().map(Map.Entry::getKey).get();
+    }
+
+    public static void main(String[] args) {
         String s = "abcdcbbace";
         char[] c = s.toCharArray();
         int d;
-        Map<Character,Integer> a = new HashMap<>();
-        for(int i=0; i<s.length(); i++) {
-            if(!a.containsKey(c[i])) {
-                a.put(c[i],1);
+        Map<Character, Integer> a = new HashMap<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (!a.containsKey(c[i])) {
+                a.put(c[i], 1);
             } else {
                 d = a.get(c[i]);
-                a.put(c[i],d+1);
+                a.put(c[i], d + 1);
             }
         }
-        for(Map.Entry<Character,Integer> m : a.entrySet()) {
-            if(m.getValue().equals(1)) {
-                System.out.println("Non repeating character is: "+m.getKey());
+        for (Map.Entry<Character, Integer> m : a.entrySet()) {
+            if (m.getValue().equals(1)) {
+                System.out.println("First Non repeating character is: " + m.getKey());
                 break;
             }
         }
+
+        System.out.println("First non repeating character using streams is: " + firstNonRepeatCharUsingStreams(s));
     }
 
 }
