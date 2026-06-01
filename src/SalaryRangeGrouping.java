06c96fb6c9c5b8242b2509f1aae442d7d8867774
Bharat Raja Savya Sachi Madishetty <bharatraja.madishetty@gmail.com>
2026-06-02 00:59:50 +0530

    Extract employee and manager pairs from the list

--- src/SalaryRangeGrouping.java
+++ src/SalaryRangeGrouping.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-record Emp(int id, String ename, double sal) {
+record Emp(int eId, String ename, double sal, Integer mgrId) {
 }
 
 enum SalaryRange {
@@ -24,11 +24,11 @@ public record SalaryRangeGrouping() {
 
     public static void main(String[] args) {
         List<Emp> l = Arrays.asList(
-                new Emp(1, "Laura", 278560),
-                new Emp(2, "Ben", 24561),
-                new Emp(3, "Stain", 78561),
-                new Emp(4, "Parker", 375619),
-                new Emp(5, "Amy", 98748));
+                new Emp(1, "Laura", 278560, null),
+                new Emp(2, "Ben", 24561, 1),
+                new Emp(3, "Stain", 78561, 1),
+                new Emp(4, "Parker", 375619, 2),
+                new Emp(5, "Amy", 98748, 3));
 
         Emp emp = l.stream().sorted((a, b) -> Double.compare(b.sal(), a.sal())).skip(2).findFirst().get();
         System.out.println("Third highest salaried employee : " + emp);
@@ -37,6 +37,13 @@ public record SalaryRangeGrouping() {
 
         var map = l.stream().collect(Collectors.groupingBy(e -> getSalaryRange(e.sal())));
         System.out.println("\nEmployees grouped within their salary range  : " + map);
+
+        l.stream().flatMap(
+                e -> l.stream().filter(m -> e.mgrId() != null && m.eId() == e.mgrId())
+                        .map(m -> m.ename() + " is the manager of " + e.ename()))
+                .forEach(System.out::println);
+
+        l.stream().filter(e -> e.mgrId() == null).forEach(e -> System.out.println(e.ename() + " is a chief manager"));
 
     }
 
